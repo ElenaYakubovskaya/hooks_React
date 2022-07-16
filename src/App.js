5b8f12b31cdf06b4013d93db0ec00a556b3e4439
Elena Yakubovskaya <elena.yakubovskaya10gmail.com>
2022-07-16 14:19:23 +0300

    gihub state done

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import { About } from './pages/About';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Alert } from './components/Alert';
 import { AlertState } from './context/alert/AlertState';
+import { GithubState } from './context/github/GithubState';
 
 function App() {
   return (
-    <AlertState>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container pt-4">
-          <Alert alert={{ text: 'test alert' }} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/profile/:name" element={<Profile />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </AlertState>
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{ text: 'test alert' }} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/profile/:name" element={<Profile />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
   );
 }
 
